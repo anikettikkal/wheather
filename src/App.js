a0bb41535b411a86eeaccd7a0172a264e460e325
anikettikkal <tikkalaniket2019@gmail.com>
2022-12-23 04:24:34 +0000

    frontend completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
+import { useState, useEffect } from "react";
 
 import './App.css';
 
 function App() {
+
+  const [city,setCity] = useState("Bangalore");
+  const [temp, setTemp] = useState(0);
+  const [description, setDescription] = useState("");
+
+
+  useEffect(()=>{
+    async function loadData(){
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3d4707888caelcef35d3b4069b3862d3`)
+      console.log(response.data);
+    }
+    loadData();
+  }, [city])
+
+
   return (
   <>
   <div className='heading col-md-12 mt-5'>
-    <h2 className='mt-4'>WHEATHER</h2>
+    <h2 className='mt-4'>WHEATHER API</h2>
   </div>
   <div className='row mt-5'>
     <div className='col-md-6 div-search'>
-      <img className='img-fluid image' src="https://as1.ftcdn.net/v2/jpg/01/52/26/16/1000_F_152261623_5O7XNZ8nT0si8ni0hUBJEnO2dAGbcsiX.jpg" />
-      <input type="text" placeholder='Enter City Name'/>
+      <img className='img-fluid image' src="https://thumbs.dreamstime.com/b/glossy-weather-stickers-16470720.jpg" />
+      <input type="text" placeholder='Enter City Name' className='input-city'
+      value={city}
+      onChange={(e) => setCity(e.target.value)}
+      />
     </div>
 
     <div className='col-md-6 display'>
-      <h1>tikkal</h1>
+      <h1>City: {city}</h1>
     </div>
 
   </div>
+
+  
   </>
   )
 }
